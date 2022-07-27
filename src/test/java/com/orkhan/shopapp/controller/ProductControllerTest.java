968cbd1560369223d60b8ef6d8d22d8d43423760
orkhan.mammadov <orkhan.mammadov@accenture.com>
2022-07-27 15:43:55 +0300

    Product apis completed with front

--- src/test/java/com/orkhan/shopapp/controller/ProductControllerTest.java
+++ src/test/java/com/orkhan/shopapp/controller/ProductControllerTest.java
@@ -1,20 +1,47 @@
 package com.orkhan.shopapp.controller;
 
+import com.orkhan.shopapp.service.ProductService;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockHttpServletResponse;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 
+@ExtendWith(SpringExtension.class)
 class ProductControllerTest {
+
+    private MockMvc mockMvc;
+
+    @InjectMocks
+    private ProductController productController;
+
+    @Mock
+    private ProductService productService;
+
+    @BeforeEach
+    public void setup(){
+        mockMvc = MockMvcBuilders.standaloneSetup(productController).build();
+    }
 
 
     @Test
     void getAll() throws Exception {
-//        when(productRepo.findAll()).thenReturn(null);
-//        MockHttpServletResponse response = mockMvc.perform(get("/").contentType(MediaType.APPLICATION_JSON))
-//                .andReturn().getResponse();
-//
-//        assertEquals(response.getStatus(), HttpStatus.OK.value());
-        assertTrue(true);
+        when(productService.getAll()).thenReturn(null);
+        MockHttpServletResponse response = mockMvc.perform(get("/api/product").contentType(MediaType.APPLICATION_JSON))
+                .andReturn().getResponse();
+
+        assertEquals(response.getStatus(), HttpStatus.OK.value());
     }
 
     @Test
